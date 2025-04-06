7361bfe819498e01c955c57d8a533304ee6b592f
Saad EL OULIKY <134555110+saadox66@users.noreply.github.com>
2025-04-06 17:41:27 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    // Disables ESLint during production builds.
-    // This will allow builds to complete even if there are lint errors.
     ignoreDuringBuilds: true,
   },
-  // Add other config options here as needed.
 };
 
 export default nextConfig;
